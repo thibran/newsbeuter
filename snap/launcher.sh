4b347a92396b1918b7026e7e6bbd5b8bebfaeeec
Thibaut Brandscheid <randaltor@web.de>
2016-12-30 14:28:23 +0100

    confinment set to 'strict' mode

--- snap/launcher.sh
+++ snap/launcher.sh
@@ -7,4 +7,8 @@ export XDG_DATA_DIRS=$SNAP_USER_DATA
 
 # must be set to empty-string to pass !setlocale(LC_CTYPE,"")
 # file: src/newsbeuter.cpp
-LANG="" LC_CTYPE="" LC_MESSAGES="" $SNAP/bin/$1
+export LANG=""
+export LC_CTYPE=""
+export LC_MESSAGES=""
+
+exec $SNAP/bin/"$@"
